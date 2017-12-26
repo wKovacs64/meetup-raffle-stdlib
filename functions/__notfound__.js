de16a049ada5a3261ee3a7967a37719d57f38b8d
Justin Hall <justin.r.hall@gmail.com>
2017-12-26 10:01:52 -0700

    Update documentation URL

--- functions/__notfound__.js
+++ functions/__notfound__.js
@@ -1,4 +1,4 @@
 module.exports = async () => ({
   message: 'Please specify a valid function.',
-  documentationUrl: 'https://stdlib.com/wKovacs64/meetup-raffle',
+  documentationUrl: 'https://stdlib.com/@wKovacs64/lib/meetup-raffle',
 });
